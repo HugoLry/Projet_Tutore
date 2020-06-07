6c9273b66373c4e1a0a4d5d8018760365fdba44b
robin-rvr <62430348+robin-rvr@users.noreply.github.com>
2020-06-08 00:22:03 +0200

    Delete connexion.php

--- connexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Permet de se connecter a la base de données mmiwork en localhost
-$link = new PDO('mysql:host=localhost;dbname=mmiwork', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-
-// Permet de se connecter a la base de données rriviere_db sur les serveurs de l'université
-//$link = new PDO('mysql:host=sqletud.u-pem.fr;dbname=rriviere_db', 'rriviere', 'hPiy7iivw8', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-
-?>
